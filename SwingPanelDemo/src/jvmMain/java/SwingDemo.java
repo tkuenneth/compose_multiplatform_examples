1dd40936c84b0600e40706052cc456385f0fc553
Thomas Künneth <thomas@thomaskuenneth.eu>
2022-11-07 16:24:41 +0100

    Refactoring

--- SwingPanelDemo/src/jvmMain/java/SwingDemo.java
+++ SwingPanelDemo/src/jvmMain/java/SwingDemo.java
@@ -2,38 +2,40 @@ import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
-
-public class SwingDemo {
+public class SwingDemo extends JFrame {
 
     public static final String[] numbers = {
             "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"
     };
 
+    private final JLabel label;
+
+    public SwingDemo() {
+        super("SwingDemo");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JComponent sliderPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+        SliderWithLabel slider = new SliderWithLabel();
+        sliderPanel.add(slider);
+        label = new JLabel();
+        label.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
+        slider.addPropertyChangeListener(SliderWithLabel.CUSTOM_PROPERTY, (event) -> updateLabel((int) event.getNewValue()));
+        updateLabel(slider.getCustomProperty());
+        JPanel contentPanel = new JPanel(new BorderLayout());
+        contentPanel.add(sliderPanel, BorderLayout.CENTER);
+        contentPanel.add(label, BorderLayout.SOUTH);
+        setContentPane(contentPanel);
+        pack();
+    }
+
+    private void updateLabel(int value) {
+        label.setText(numbers[value - 1]);
+    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame main = new JFrame("SwingDemo");
-            main.setDefaultCloseOperation(EXIT_ON_CLOSE);
-            JComponent sliderPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
-            SliderWithLabel slider = new SliderWithLabel();
-            sliderPanel.add(slider);
-            JLabel label = new JLabel();
-            label.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
-            slider.addPropertyChangeListener(SliderWithLabel.CUSTOM_PROPERTY, (event) -> {
-                updateLabel(label, (int) event.getNewValue());
-            });
-            updateLabel(label, slider.getCustomProperty());
-            JPanel contentPanel = new JPanel(new BorderLayout());
-            contentPanel.add(sliderPanel, BorderLayout.CENTER);
-            contentPanel.add(label, BorderLayout.SOUTH);
-            main.setContentPane(contentPanel);
-            main.pack();
+            SwingDemo main = new SwingDemo();
             main.setLocationRelativeTo(null);
             main.setVisible(true);
         });
-    }
-
-    private static void updateLabel(JLabel label, int value) {
-        label.setText(numbers[value - 1]);
     }
 }
